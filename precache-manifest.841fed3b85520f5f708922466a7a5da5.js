ae27ea4c893ea73838e3c821603f33029036273d
Beatricce <beatriz.de.oli.s@gmail.com>
2020-06-05 01:06:48 -0300

    Updates

---
+++ precache-manifest.841fed3b85520f5f708922466a7a5da5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9453b869e433c6367ffc5038e42d953c",
+    "url": "/dashboardReact/index.html"
+  },
+  {
+    "revision": "42782807a71351f81f3f",
+    "url": "/dashboardReact/static/css/2.4deadad5.chunk.css"
+  },
+  {
+    "revision": "c6bd26b3c581a66b0948",
+    "url": "/dashboardReact/static/css/main.50695a3d.chunk.css"
+  },
+  {
+    "revision": "42782807a71351f81f3f",
+    "url": "/dashboardReact/static/js/2.dd94ac28.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dashboardReact/static/js/2.dd94ac28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6bd26b3c581a66b0948",
+    "url": "/dashboardReact/static/js/main.4aba0db6.chunk.js"
+  },
+  {
+    "revision": "846a28e7c19c4f5b663b",
+    "url": "/dashboardReact/static/js/runtime-main.4fd82981.js"
+  }
+]);
